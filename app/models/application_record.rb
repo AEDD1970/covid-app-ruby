9cbf214854035c86161d585de4044751b1ae86b9
Brandon Barrera <brandon.barrera@hrinteractive.co>
2020-05-12 12:31:55 -0500

    heroku time zone

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  config.time_zone = 'America/Bogota'
+  config.active_record.default_timezone = "utf-8" # Or :utc
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,4 @@
 # Load the Rails application.
 require_relative 'application'
-
-Rails.application.configure do
-  config.time_zone = 'America/Bogota'
-  config.active_record.default_timezone = :local
-end
-
 # Initialize the Rails application.
 Rails.application.initialize!
