d131c6067565a6eeac73301aa2baf4bd199f4cf5
Brandon Barrera <brandon.barrera@hrinteractive.co>
2020-05-12 12:20:25 -0500

    heroku time zone

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  config.time_zone = 'America/Bogota'
-  config.encoding = "utf-8"
-
-
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+config.time_zone = 'America/Bogota'
+config.encoding = "utf-8"
+end
